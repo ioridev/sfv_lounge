16bd444e81ffa2e7c9f40df6807aaa1479e42b12
iori <iori@iori.dev>
2021-06-14 06:11:16 +0900

    募集ページの作成

--- lib/view/tweetpage.dart
+++ lib/view/tweetpage.dart
@@ -1,7 +1,3 @@
-import 'dart:async';
-
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 
 class TweetPage extends StatefulWidget {
@@ -18,8 +14,38 @@ class _TweetPageState extends State<TweetPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: ListView(children: <Widget>[]),
+      appBar: AppBar(
+        title: const Text('募集する'),
+        centerTitle: true,
+        actions: <Widget>[IconButton(icon: Icon(Icons.help), onPressed: null)],
+      ),
+      body: Center(
+          child: Column(children: <Widget>[
+        Column(children: <Widget>[
+          Text('【ID】'),
+          TextField(),
+        ]),
+        Column(children: <Widget>[
+          Text('【キャラ】'),
+          TextField(),
+        ]),
+        Column(children: <Widget>[
+          Text('【LP】'),
+          TextField(),
+        ]),
+        Column(children: <Widget>[
+          Text('【ルール】'),
+          TextField(),
+        ]),
+        Column(children: <Widget>[
+          Text('【パス】'),
+          TextField(),
+        ]),
+        Column(children: <Widget>[
+          Text('【コメント】'),
+          TextField(),
+        ]),
+      ])),
     );
   }
 }
